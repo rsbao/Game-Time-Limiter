900ba65394e90a5eed0472fe09e0d56b8a722c3a
Ishaan514 <esiddaling@gmail.com>
2021-03-20 18:39:48 -0400

    desktop done

---
+++ sample-app-master/ts/src/consts.ts
@@ -0,0 +1,25 @@
+const leagueClassId = 5426;
+
+const interestingFeatures = [
+    'game_data',
+    'live_client_data',
+    'death',
+    'gameTime'
+    
+];
+
+const windowNames = {
+    inGame: 'in_game',
+    desktop: 'desktop'
+};
+
+const hotkeys = {
+    toggle: 'showhide'
+};
+
+export {
+    leagueClassId,
+    interestingFeatures,
+    windowNames,
+    hotkeys
+}
